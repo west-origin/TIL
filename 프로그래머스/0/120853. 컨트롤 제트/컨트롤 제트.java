293ef48dbae88bd605d365639de175577f865409
west-origin <115988606+west-origin@users.noreply.github.com>
2025-02-04 22:52:45 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.14 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        String[] list = s.split(" ");
+        int answer = 0;
+        for(int i = 0 ; i < list.length ;i++){
+            if(list[i].equals("Z")){
+                answer -= Integer.parseInt(list[i-1]);  
+            } else {
+                answer += Integer.parseInt(list[i]);  
+            }
+        }
+        return answer;
+    }
+}
