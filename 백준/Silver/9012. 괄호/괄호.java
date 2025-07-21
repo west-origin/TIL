3d0fc6ed82a35570663b78722ad2dd873ae72b71
west-origin <115988606+west-origin@users.noreply.github.com>
2025-07-21 21:28:48 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,39 +1,34 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+
  
 public class Main {
- 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        for(int i=0; i<t; i++) {
-            sb.append(solve(br.readLine())).append('\n');
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++) {
+            System.out.println(solution(br.readLine()));
         }
-        System.out.println(sb);
     }
     
-    public static String solve(String s) {
+    public static String solution(String str) {
+        
         Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(c=='(') {
-                stack.push(c);
-            } else if(stack.empty()) {
-                return "NO";
+        
+        for(int j=0 ; j<str.length(); j++) {
+            if(str.charAt(j)=='(') {
+                stack.push(str.charAt(j));
             } else {
+                if(stack.empty()) {
+                    return "NO";
+                }
                 stack.pop();
             }
         }
-        
-        if(stack.empty()) {
-            return "YES";
-        } else {
-            return "NO";
-        }
+        return stack.empty()?"YES":"NO";
     }
-    
-    
 }
