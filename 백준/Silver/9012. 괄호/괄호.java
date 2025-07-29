63c3f58c6703b716489c7ba60de9335dcb6a2a6f
west-origin <115988606+west-origin@users.noreply.github.com>
2025-07-29 21:58:12 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,34 +1,45 @@
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
- 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        for(int i=0; i<n; i++) {
-            System.out.println(solution(br.readLine()));
-        }
-    }
-    
-    public static String solution(String str) {
+        StringBuilder sb = new StringBuilder();
         
+        
+        for(int i = 0 ; i < n ; i++) {
+            sb.append(solve(br.readLine())).append('\n');
+        }
+        
+        System.out.println(sb);
+    }
+    /**
+     * NO 경우의 수 
+     * 1. (((((((((((만 반복할 경우
+     * 2. stack이 없을 때 )가 될 경우
+     * 3. 
+     */
+    public static String solve(String str) {
         Stack<Character> stack = new Stack<>();
         
-        for(int j=0 ; j<str.length(); j++) {
-            if(str.charAt(j)=='(') {
-                stack.push(str.charAt(j));
-            } else {
-                if(stack.empty()) {
+        for(int i = 0 ; i < str.length(); i++) {
+            if(str.charAt(i) == '(') {
+                stack.add('(');
+            } else if(str.charAt(i) == ')') {
+                if(stack.size() == 0) { 
                     return "NO";
+                } else {
+                    stack.pop();
                 }
-                stack.pop();
             }
         }
-        return stack.empty()?"YES":"NO";
+        
+        if(stack.size() != 0) { // 안에 남아 있는 경우
+            return "NO";
+        } else {
+            return "YES";
+        }
     }
 }
