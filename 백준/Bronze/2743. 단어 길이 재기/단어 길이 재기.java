094bf47ecbabce3ec2be8a80120d1103556ac0e0
west-origin <115988606+west-origin@users.noreply.github.com>
2025-08-28 21:48:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+    }
+}
